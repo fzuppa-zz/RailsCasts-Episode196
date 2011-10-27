b2675441027e147f62d35a3bcf8bfeff69a4dc28
fzuppa <fzuppa@gmail.com>
2011-10-27 22:25:40 +0100

    Railscasts episode 196

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Surverysays::Application.config.secret_token = '19327f23f13f12a904a206ea31de3ced4cc3164e76ee34f07fb93624578f5f3b344c617934e779b7c379d46997672c487b4a889c5f184f73bc1007a25e0fac92'
